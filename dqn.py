de715e7ec7dc5cc403d03a1fbdcc9360f33ade15
Shayaan Abdullah <sabdullah@berkeley.edu>
2017-08-20 22:43:46 +0000

    Update skeleton code for DQN

--- dqn.py
+++ dqn.py
@@ -1,63 +1,149 @@
 import tensorflow as tf
-
+import random as rand
+import numpy as np 
 # so perhaps we perform a rollout prior to actually running the 
 # a basic implementation of dqn for the cartpole environment
 # ultimately we will have a function that actually runs the episode
 
-def run_episode(env,
-                max_steps=200,
-                ):
-    """
-    :param env: An environment object
-    :param max_steps: The number of steps to run the episode over
-    :return: The total reward over the episode
-    """
-    # the first thing to do is to reset the environment and get the
-    # corresponding observation
-    observation = env.reset()
-    # initialize the total reward to 0 to begin with
-    total_reward = 0
-    # now step through the episode compute the actions and continue
-    for _ in range(max_steps):
-        # this is where we will get the action
-        action = action_fn(observation)
-        # once we have an action we can observe its affect on the environment
-        observations, reward, done, info = env.step(action)
+def main(buffer_size,
+        epsilon,
+        target_update,
+        num_episodes,
+        env):
+    # first thing to do is to initialize the buffer
+    # we can also specify the maximum size of the buffer before initializing
+    # it
+    replay_buffer = ExperienceReplay(buffer_size)
+    # also initialize a Q object
+    q_function = QFunction()
+    # initialize a counter for the number of steps taken
+    step_counter = 0 
+    for episode_num in range(num_episodes):
+        # we will now loop through the steps in each episode
+        # at the start of each episode reset the environment 
+        observation = env.reset()
+        # also set the total reward in the episode to 0
+        total_reward = 0
+        for step in range(episode_length):
+            # increment step counter
+            step_counter += 1
+            # presumption is that a random number is generated between 0 and 1
+            # then with probability epsilon we will select a random action
+            if rand.random() < epsilon:
+                # define an action space which will be sampled for random actions
+                action = action_space.sample()
+            else:
+                # on the other hand with 1 - epsilon probability select an
+                # action as the action that yields the maximum q value over the
+                # action space keep this should make use of the target Q function
+                action = q_function.find_action(observation)
+        # now that we have an action we can execute it in the emulator
+        new_observation, reward, done, info = env.step(action)
+        # increment the total reward in the episode using the step
         total_reward += reward
-        # if the the episode goal has been achieved break out of the episode
-        # at what point should we update the q function
+        # we want to store the transisition tuple in the buffer
+        replay_buffer.add((observation, action, reward, new_observation, 
+                            done))
+        # at this point we update the action_value function if the buffer
+        # crosses some threshold
+        if replay_buffer.size > threshold:
+            # update the Qfunction by passing it a minibatch of transitions
+            transitions = replay_buffer.sample(batch_size)
+            # transisitions can just be a list of tuples where the list is of
+            # length batch_size
+            q_function.train(transitions)
+        # update the target Q function if that threshold has been reached
+        if step_counter % target_update == 0:
+            q_function.update_target()
+        # if the episode has reached completion we can break out of it and
+        # reset the episode
         if done:
             break
-    return total_reward
-    
-def action_fn(observation):
-    """
-    Given an observation provide an action to apply to the environment.
-    The action space in this case is of dimension 2
-    :param observation: A 1d array of shape (4,)
-    :return A scalar which is either a 1 or 0
-    """
-    # the first thing is to compute the action value function over the
-    # set of possible actions that will be used a correct action
-    q_vals = q_fn(observation)
-    # q_vals should be of shape [2] where the first element will correspond
-    # to the Q val for the action 0 and the second q value for the action
-    # corresponding to 1
-    return np.argmax(q_vals)
-    
-def q_fn(observation):
-    """
-    Compute the q values for the provided observation this is equivalent to
-    just using the correct
-    """
-    
-class HParams(object):
-    """
-    Specify the hyperparameters for the DQN model
-    """
+        # at the of the the new observation should become the current obs
+        observation = new_observation
 
 class ExperienceReplay(object):
     """
-    Define an object that holds the previously encountered transistions
+    Experience buffer to keep track of environment transitions and provide
+    samples for training
     """
+    def __init__(self,
+                 max_size):
+        """
+        :param max_size: The maximum size of the buffer
+        """
+        # perhaps initialize the arrays based on the provided input
+        
+    def add(self,
+            transition_tuple):
+        """
+        Add a single transition tuple to the buffer
+        :param transition_tuple: A tuple of form 
+            (observation, action, reward, next_observation, done)
+        :return : None
+        """
+        return NotImplemented
+        
+    def sample(self,batch_size):
+        """
+        Sample a sequence of transitions from the replay buffer
+        :param batch_size: An integer specifying the number of transistions to
+                sample
+        :return A list of transition tuples of length equal to batch_size
+        """
+        return NotImplemented
+        
+class QFunction(object):
+    """
+    An object that represents the action value function that will be used to
+    choose actions by computing action values on the target Q function
+    """
+    def __init__(self):
+        """
+        Perhaps build the Tensorflow graphs here
+        """
+
+        
+    def find_action(self,
+                    observation):
+        """
+        Find the action with the highest q value by evaluating the target_qfn
+        on the 
+        :param observation: A numpy array of shape [observation_dim]
+        :return A numpy array of shape [action_dim]
+        """
+        raise NotImplementedError
+        
+    def train(self, transitions):
+        """
+        Update the parameters of the surrogate Q function using the
+        Bellman update
+        """
+        
+    def update_target(self):
+        """
+        Update the parameters of the target Q function by copying over the
+        parameters of the surrogate Q function 
+        """
+        
+class Model(object):
+    """
+    The Tensorflow graph that represents the model that is being used to
+    compute action values 
+    """
+    
+    def __init__(self, scope):
+        """
+        initialize some model attributes that will be useful later
+        """
+        self.scope = scope
+    
+    def build_graph(self):
+        """
+        Build a tensorflow graph in the given scope
+        return an op dict that will be used to create the
+        """
+    
+
+        
     
